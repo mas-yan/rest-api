85ec8e94db1e678bbfc341bccd0253d62d527778
mas-yan <mrey5597@gmail.com>
2021-09-10 17:27:15 +0700

    create login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $token = Auth::attempt($request->only('email', 'password'));
+        if (!$token) {
+            return response(null, 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('register', RegisterController::class);
+Route::post('login', LoginController::class);
+Route::get('user', UserController::class);
