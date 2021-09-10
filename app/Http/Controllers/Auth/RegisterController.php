8b924e29b2e474a201e8d2b36c390035a41d00eb
mas-yan <mrey5597@gmail.com>
2021-09-10 16:26:12 +0700

    create register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        request()->validate([
+            'name' => ['string', 'required'],
+            'username' => ['alpha_num', 'required', 'min:3', 'max:25', 'unique:users,username'],
+            'email' => ['email', 'required', 'unique:users,email'],
+            'password' => ['required', 'min:6', '']
+        ]);
+
+        $register = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'thanks for registered',
+            'data' => $register
+        ], 200);
+    }
+}
