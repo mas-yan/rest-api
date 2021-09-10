e235f87e444fb363e3ec6762f76220e3176cac1e
mas-yan <mrey5597@gmail.com>
2021-09-10 18:15:22 +0700

    create logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -24,3 +25,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', RegisterController::class);
 Route::post('login', LoginController::class);
 Route::get('user', UserController::class);
+Route::post('logout', LogoutController::class);
